fffcaa4ecd9bc062b1358d32973e8da46523b1dd
Fredrik Bagge Carlson <cont-frb@ulund.org>
2019-05-23 07:39:00 +0200

    Convenience constructor

--- src/utils.jl
+++ src/utils.jl
@@ -138,10 +138,6 @@ Base.:(\)(a::PDMats.PDMat, x::StaticVector) = a.chol \ x
 PDMats.invquad(a::PDMats.PDiagMat, x::StaticVector) = PDMats.wsumsq(a.inv_diag, x)
 
 
-# product distributions
-@generated function Distributions._logpdf(d::Product, x::StaticVector{N}{<:Real}) where N
-    :(Base.Cartesian.@ncall $N Base.:+ i->logpdf(d.v[i], x[i]))
-end
 
 """
 Mixed value support indicates that the distribution is a mix of continuous and discrete dimensions.
@@ -163,6 +159,7 @@ struct TupleProduct{N,S,V<:NTuple{N,UnivariateDistribution}} <: MultivariateDist
         return new{N,Mixed,V}(v)
     end
 end
+TupleProduct(d::Distribution...) = TupleProduct(d)
 Base.length(d::TupleProduct{N}) where N = N
 # Distributions._rand!(rng::AbstractRNG, d::TupleProduct, x::AbstractVector{<:Real}) =     broadcast!(dn->rand(rng, dn), x, d.v)
 
@@ -176,6 +173,12 @@ end
 @generated function Distributions._logpdf(d::TupleProduct{N}, x::AbstractVector{<:Real}) where N
     :(Base.Cartesian.@ncall $N Base.:+ i->logpdf(d.v[i], x[i]))
 end
+
+# To make it a bit faster also for the regular Product
+@generated function Distributions._logpdf(d::Product, x::StaticVector{N}{<:Real}) where N
+    :(Base.Cartesian.@ncall $N Base.:+ i->logpdf(d.v[i], x[i]))
+end
+
 Distributions.mean(d::TupleProduct) = vcat(mean.(d.v)...)
 Distributions.var(d::TupleProduct) = vcat(var.(d.v)...)
 Distributions.cov(d::TupleProduct) = Diagonal(var(d))
