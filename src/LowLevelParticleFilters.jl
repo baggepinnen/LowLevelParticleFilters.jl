7c61c8b9b8cf9bd6821d554dc3d4f6e57186cd50
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2020-02-22 18:33:38 +0800

    Fix requires

--- src/LowLevelParticleFilters.jl
+++ src/LowLevelParticleFilters.jl
@@ -25,8 +25,10 @@ include("plotting.jl")
 
 index(f::AbstractFilter) = f.t[]
 
-function Base.__init__()
-@require Plots = "91a5bcdd-55d7-5caf-9e0b-520d859cae80" include("plots.jl")
+
+using Requires
+function __init__()
+    @require Plots = "91a5bcdd-55d7-5caf-9e0b-520d859cae80" include("plots.jl")
 end
 
 end # module
