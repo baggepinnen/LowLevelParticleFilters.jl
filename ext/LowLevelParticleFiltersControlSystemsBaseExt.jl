4198e5fe98b42b452bb539cedc9eeb9ff4e005a8
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2023-08-29 14:04:47 +0200

    fix double import

--- ext/LowLevelParticleFiltersControlSystemsBaseExt.jl
+++ ext/LowLevelParticleFiltersControlSystemsBaseExt.jl
@@ -1,6 +1,8 @@
 module LowLevelParticleFiltersControlSystemsBaseExt
 import LowLevelParticleFilters.KalmanFilter
-using ControlSystemsBase: AbstractStateSpace, Discrete, ssdata
+using LowLevelParticleFilters: AbstractExtendedKalmanFilter, AbstractUnscentedKalmanFilter
+using ControlSystemsBase: AbstractStateSpace, ssdata
+import ControlSystemsBase
 using Distributions
 
 """
@@ -8,9 +10,15 @@ using Distributions
 
 Construct a `KalmanFilter` from a predefined `StateSpace` system from ControlSystems.jl
 """
-function KalmanFilter(sys::AbstractStateSpace{<:Discrete}, R1, R2, d0=MvNormal(Matrix(R1)); kwargs...)
+function KalmanFilter(sys::AbstractStateSpace{<:ControlSystemsBase.Discrete}, R1, R2, d0=MvNormal(Matrix(R1)); kwargs...)
     A, B, C, D = ssdata(sys)
-    kf = KalmanFilter(A, B, C, D, Matrix(R1), Matrix(R2), d0; kwargs...)
+    KalmanFilter(A, B, C, D, Matrix(R1), Matrix(R2), d0; kwargs...)
+end
+
+function ControlSystemsBase.linearize(kf::Union{AbstractExtendedKalmanFilter, AbstractUnscentedKalmanFilter}, x::AbstractVector, u::AbstractVector, p, t)
+    A,B = ControlSystemsBase.linearize(kf.dynamics, x, u, p, t)
+    C,D = ControlSystemsBase.linearize(kf.measurement, x, u, p, t)
+    (; A, B, C, D)
 end
 
 end
